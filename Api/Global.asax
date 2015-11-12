14f197f93338e7f94df60a955ffb594546b676be
Simon Rigby <simon.rigby@levelnis.co.uk>
2015-11-12 23:03:24 +0000

    Initial commit

---
+++ Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Levelnis.Learning.CallingWebApiFromMvc.Api.WebApiApplication" Language="C#" %>
---
+++ Api/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿namespace Levelnis.Learning.CallingWebApiFromMvc.Api
+{
+    using System.Web.Http;
+    using System.Web.Mvc;
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+    }
+}
