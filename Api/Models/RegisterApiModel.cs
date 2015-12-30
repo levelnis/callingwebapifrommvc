2ffe7d1ca10979035e1b7a7a1f59194e9ab7498c
Simon Rigby <simon.rigby@levelnis.co.uk>
2015-12-30 18:52:04 +0000

    Added some basic validation to register

--- Api/Models/RegisterApiModel.cs
+++ Api/Models/RegisterApiModel.cs
@@ -5,12 +5,16 @@
     public class RegisterApiModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
 
         [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
--- Web/Models/RegisterViewModel.cs
+++ Web/Models/RegisterViewModel.cs
@@ -1,9 +1,17 @@
 ﻿namespace Levelnis.Learning.CallingWebApiFromMvc.Web.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class RegisterViewModel
     {
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
     }
 }
